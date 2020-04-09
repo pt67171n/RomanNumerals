3f96009e06501f03a654afce4add503304b9a9ce
pt67171n <63216109+pt67171n@users.noreply.github.com>
2020-04-08 20:31:10 -0400

    Add files via upload

---
+++ Roman Numerals.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+class py_solution:
+    def int_to_Roman(self, num):
+        val = [
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4,
+            1
+            ]
+        syb = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV",
+            "I"
+            ]
+        roman_num = ''
+        i = 0
+        while  num > 0:
+            for _ in range(num // val[i]):
+                roman_num += syb[i]
+                num -= val[i]
+            i += 1
+        return roman_num
+
+
+    def roman_to_int(self, s):
+
+      rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+      int_val = 0
+      for i in range(len(s)):
+          if i > 0 and rom_val[s[i]] > rom_val[s[i - 1]]:
+              int_val += rom_val[s[i]] - 2 * rom_val[s[i - 1]]
+          else:
+              int_val += rom_val[s[i]]
+      return int_val
+
+
+print(py_solution().roman_to_int('MMMM'))
+print(py_solution().roman_to_int('C'))
+print(py_solution().int_to_Roman(4033))
+
